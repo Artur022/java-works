7ff032ff49d5de77bd367b91f5d30ad2bbee1890
Desmoond <artur02288@yandex.ru>
2019-05-05 15:32:19 +0300

    Урок 4 Редактирование комментариев в калькуляторе! v2.0.2

--- untitled/src/Calculator.java
+++ untitled/src/Calculator.java
@@ -1,36 +1,34 @@
 public class Calculator {
 
+    /* Свойство хранящее результат операции */
     private int result;
 
-    /* + */
+    /* Операция сложения */
     public void add (int a, int b) {
             this.result = a+b;
     }
 
-    /* - */
-
+    /* Операция вычитания */
     public void minus (int a, int b) {
         this.result = a-b;
     }
 
-    /* * */
-
+    /* Операция умножения */
     public void umnoz (int a, int b) {
             this.result = a*b;
     }
 
-    /* / */
-
+    /* Операция деления */
     public void div (int a, int b) {
             this.result = a/b;
     }
 
-    /* ^ */
-
+    /* Возведение в степень */
     public void mStpn (int a, int b) {
             this.result = Stepen(a,b);
     }
 
+    /* Метод возведения числа A в степень B. Можно вместо нее использовать Math.Power*/
     public static int Stepen(int a, int b) {
 
         int first = a;
@@ -42,11 +40,12 @@ public class Calculator {
         return result;
     }
 
-
+    /* Метод получения результата */
     public int getResult() {
         return this.result;
     }
 
+    /* Метод для очисткики резульатата */
     public void cleanResult() {
         this.result = 0;
     }
--- untitled/src/InteractRunner.java
+++ untitled/src/InteractRunner.java
@@ -1,27 +1,45 @@
+/* Импорт модуля для работы Scanner'a */
 import java.util.Scanner;
 
 public class InteractRunner {
 
+    /* Главный метод main */
     public static void main(String[] args) {
 
+        /* Создание объекта класса Scanner */
         Scanner reader = new Scanner(System.in);
 
+
         try {
+
+            /* Создание объекта класса Calculator */
             Calculator calc = new Calculator();
+
+            /* Инициализация псевдо-флагов для контроля сохранения результата и выхода из программы*/
             String exit = "no";
             String fSave = "no";
+
+            /* Цикл, пока пользователь не сказал заканчивать */
             while (!exit.equals("yes")) {
 
                 System.out.println("Hello! It is Calculator for DCP Peoples v2.0");
+
+                /* Спрашиваем и запоминаем первый аргумент операции */
                 System.out.println("Enter first arg : ");
                 Integer first = Integer.valueOf(reader.next());
 
+                /* Цикл, пока пользователь не указал, чтобы НЕ сохранять результат */
                 while (!fSave.equals("yes")) {
+
+                    /* Спрашиваем и запоминаем второй аргумент */
                     System.out.println("Enter second arg : ");
                     Integer second = Integer.valueOf(reader.next());
+
+                    /* Спрашиваем и запоминаем действие операции */
                     System.out.println("Select operation +,-,*,/,^");
                     String operation = reader.next();
 
+                    /* Выбор нужной операции */
                     switch (operation) {
                         case "+":
                             calc.add(first,second);
@@ -38,26 +56,38 @@ public class InteractRunner {
                         case "^":
                             calc.mStpn(first,second);
                             break;
+                        /* На случай, если пользователь указал несуществующую операцию */
                         default:
                             System.out.println("Error operation! This operation is not found! Try again or close select 'yes' for exit!");
                             break;
                     }
 
+                    /* Формируем результатирующую строку */
                     System.out.println("Result : " + first + operation + second + " = " + calc.getResult());
+
+                    /* Спрашиваем нужно ли запоминать результат */
                     System.out.println("Save last result? yes/no");
                     String save = reader.next();
+
+                    /* Запоминаем последний результат, если пользователь ответил "yes" */
                     if (save.equals("yes")) {
-                        System.out.println("Save last result!");
+                        System.out.println("Save last result...");
+
+                        /* Запоминаем последний результат в переменную First для дальнейшего использования */
                         first = calc.getResult();
+
+                        /* Чистим свойство результата */
                         calc.cleanResult();
                     }
                 }
 
+                /* Спрашиваем выходить или нет */
                 System.out.println("exit yes/no");
                 exit = reader.next();
 
             }
         } finally {
+            /* Закрываем Reader */
             reader.close();
         }
 
