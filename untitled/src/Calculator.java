347b30123ec76668ab3d877f475178660df914cb
Desmoond <artur02288@yandex.ru>
2019-05-05 15:20:01 +0300

    Урок 4 Исправление калькулятора! v2.0.1

--- untitled/src/Calculator.java
+++ untitled/src/Calculator.java
@@ -2,11 +2,46 @@ public class Calculator {
 
     private int result;
 
-    public void add (int ... params) {
-        for (Integer param: params) {
-            this.result += param;
-        }
+    /* + */
+    public void add (int a, int b) {
+            this.result = a+b;
     }
+
+    /* - */
+
+    public void minus (int a, int b) {
+        this.result = a-b;
+    }
+
+    /* * */
+
+    public void umnoz (int a, int b) {
+            this.result = a*b;
+    }
+
+    /* / */
+
+    public void div (int a, int b) {
+            this.result = a/b;
+    }
+
+    /* ^ */
+
+    public void mStpn (int a, int b) {
+            this.result = Stepen(a,b);
+    }
+
+    public static int Stepen(int a, int b) {
+
+        int first = a;
+        int stpn = b;
+        int result = first;
+        for (int i = 1; i<stpn; i++){
+            result = result * first;
+        }
+        return result;
+    }
+
 
     public int getResult() {
         return this.result;
--- untitled/src/InteractRunner.java
+++ untitled/src/InteractRunner.java
@@ -9,17 +9,53 @@ public class InteractRunner {
         try {
             Calculator calc = new Calculator();
             String exit = "no";
+            String fSave = "no";
             while (!exit.equals("yes")) {
+
+                System.out.println("Hello! It is Calculator for DCP Peoples v2.0");
                 System.out.println("Enter first arg : ");
-                String first = reader.next();
-                System.out.println("Enter second arg : ");
-                String second = reader.next();
-                calc.add(Integer.valueOf(first),Integer.valueOf(second));
-                System.out.println("Result : " + calc.getResult());
-                calc.cleanResult();
+                Integer first = Integer.valueOf(reader.next());
+
+                while (!fSave.equals("yes")) {
+                    System.out.println("Enter second arg : ");
+                    Integer second = Integer.valueOf(reader.next());
+                    System.out.println("Select operation +,-,*,/,^");
+                    String operation = reader.next();
+
+                    switch (operation) {
+                        case "+":
+                            calc.add(first,second);
+                            break;
+                        case "-":
+                            calc.minus(first,second);
+                            break;
+                        case "*":
+                            calc.umnoz(first,second);
+                            break;
+                        case "/":
+                            calc.div(first,second);
+                            break;
+                        case "^":
+                            calc.mStpn(first,second);
+                            break;
+                        default:
+                            System.out.println("Error operation! This operation is not found! Try again or close select 'yes' for exit!");
+                            break;
+                    }
+
+                    System.out.println("Result : " + first + operation + second + " = " + calc.getResult());
+                    System.out.println("Save last result? yes/no");
+                    String save = reader.next();
+                    if (save.equals("yes")) {
+                        System.out.println("Save last result!");
+                        first = calc.getResult();
+                        calc.cleanResult();
+                    }
+                }
+
                 System.out.println("exit yes/no");
                 exit = reader.next();
-                System.out.println("Calculator Dlya Daunov v2.0");
+
             }
         } finally {
             reader.close();
